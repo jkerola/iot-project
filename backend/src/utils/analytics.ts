e657b4aad93e1deb14c1a339f0c18c48c5612053
Niko Heikkilä <niko.heikkila13@gmail.com>
2023-01-05 18:19:48 +0200

    added separate limits for restaurants

--- backend/src/utils/analytics.ts
+++ backend/src/utils/analytics.ts
@@ -3,11 +3,45 @@
  * Values taken from https://smartcampus.oulu.fi/manage/ grafana view.
  */
 const limits = {
-    co2: {
-        low: 450,
-        moderate: 500,
-        high: 550,
-        veryHigh: 600,
+    napa: {
+        co2: {
+            low: 425,
+            moderate: 475,
+            high: 525,
+            veryHigh: 575,
+        },
+    },
+    kastari: {
+        co2: {
+            low: 475,
+            moderate: 525,
+            high: 575,
+            veryHigh: 625,
+        },
+    },
+    foodoo: {
+        co2: {
+            low: 500,
+            moderate: 550,
+            high: 600,
+            veryHigh: 650,
+        },
+    },
+    mara: {
+        co2: {
+            low: 400,
+            moderate: 450,
+            high: 500,
+            veryHigh: 550,
+        },
+    },
+    foobar: {
+        co2: {
+            low: 450,
+            moderate: 500,
+            high: 550,
+            veryHigh: 600,
+        },
     },
 };
 
@@ -26,14 +60,20 @@ const calculateAverage = (list: Array<number>): number => {
  * @param averageCO2 number representing the average co2 level
  * @returns String representing estimate crowd-level
  */
-const estimateCrowd = (averageCO2: number): string => {
-    if (averageCO2 < limits.co2.low) {
+const estimateCrowd = (averageCO2: number, restaurant: string): string => {
+    if (averageCO2 < limits[restaurant as keyof typeof limits].co2.low) {
         return "very low";
-    } else if (averageCO2 < limits.co2.moderate) {
+    } else if (
+        averageCO2 < limits[restaurant as keyof typeof limits].co2.moderate
+    ) {
         return "low";
-    } else if (averageCO2 < limits.co2.high) {
+    } else if (
+        averageCO2 < limits[restaurant as keyof typeof limits].co2.high
+    ) {
         return "moderate";
-    } else if (averageCO2 < limits.co2.veryHigh) {
+    } else if (
+        averageCO2 < limits[restaurant as keyof typeof limits].co2.veryHigh
+    ) {
         return "high";
     } else {
         return "very high";
